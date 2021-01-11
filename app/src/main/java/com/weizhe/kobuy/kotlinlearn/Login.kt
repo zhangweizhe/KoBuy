b1be9cb3a4e77b13b922d1d44aab645bcd2b85e0
张伟哲 <704052130@qq.com>
2021-01-11 23:15:28 +0800

    login 练习

---
+++ app/src/main/java/com/weizhe/kobuy/kotlinlearn/Login.kt
@@ -0,0 +1,19 @@
+package com.weizhe.kobuy.kotlinlearn
+
+fun main() {
+    doLogin("123", "qwe") {
+        if (it) {
+            println("login success")
+        }else{
+            println("login fail")
+        }
+    }
+}
+
+fun doLogin(userName:String, password:String, onResult: (Boolean) -> Unit) {
+    if ("123" == userName && "qwe" == password) {
+        onResult(true)
+    }else{
+        onResult(false)
+    }
+}
