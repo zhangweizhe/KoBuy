723cfdaf29cf3abcd1ebc55f5fbb41137318e6ee
张伟哲 <704052130@qq.com>
2021-01-19 23:05:56 +0800

    ConstrainLayout 实现 FlowLayout 动态添加删除子view

--- app/src/main/java/com/weizhe/kobuy/constraint/FlowFragment.java
+++ app/src/main/java/com/weizhe/kobuy/constraint/FlowFragment.java
@@ -1,22 +1,34 @@
 package com.weizhe.kobuy.constraint;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.helper.widget.Flow;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.internal.FlowLayout;
 import com.weizhe.kobuy.R;
 
 public class FlowFragment extends Fragment {
 
     private RadioGroup wrapModeRG;
     private Flow flow;
+    private ConstraintLayout flowLayout;
+    private EditText addTagET;
+    private Button addTagBtn;
+    private Button deleteTagBtn;
 
     @Nullable
     @Override
@@ -29,6 +41,32 @@ public class FlowFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         wrapModeRG = view.findViewById(R.id.rg_wrap_mode);
         flow = view.findViewById(R.id.constraint_flow);
+        flowLayout = view.findViewById(R.id.flow_layout);
+        addTagET = view.findViewById(R.id.et_tag_content);
+        addTagBtn = view.findViewById(R.id.btn_add_tag);
+        deleteTagBtn = view.findViewById(R.id.btn_delete_tag);
+        addTagBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTag();
+            }
+        });
+        addTagET.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keycode, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && keycode == KeyEvent.KEYCODE_ENTER) {
+                    addTag();
+                    return true;
+                }
+                return false;
+            }
+        });
+        deleteTagBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeTag();
+            }
+        });
         wrapModeRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -50,5 +88,27 @@ public class FlowFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void addTag() {
+        String tagContent = addTagET.getText().toString().trim();
+        if (TextUtils.isEmpty(tagContent)) {
+            return;
+        }
+        Button button = (Button) getLayoutInflater().inflate(R.layout.one_button, null);
+        button.setText(tagContent);
+        button.setId(View.generateViewId());
+
+        flowLayout.addView(button);
+        flow.addView(button);
+    }
+
+    private void removeTag() {
+        View lastChild = flowLayout.getChildAt(flowLayout.getChildCount() - 1);
+        if (lastChild == null) {
+            return;
+        }
+        flowLayout.removeView(lastChild);
+        flow.removeView(lastChild);
     }
 }
